f28f3877971437baabf588413836ac1883c2288d
ppresnyakov <ppresnyakov@yandex.ru>
2021-10-10 23:05:37 +0300

    Initial commit

---
+++ src/example/HelloWorldClient.java
@@ -0,0 +1,16 @@
+package example;
+
+import org.apache.axis.client.Call;
+import org.apache.axis.client.Service;
+import javax.xml.namespace.QName;
+import java.net.URL;
+
+
+public class HelloWorldClient {
+  public static void main(String[] argv) {
+
+  }
+
+}
+
+
